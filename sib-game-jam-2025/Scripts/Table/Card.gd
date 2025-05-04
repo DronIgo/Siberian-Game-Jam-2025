116c088ca45c37852303b2ccf6f0a8c56a9bb067
Daniil Beloglazov <zheltogsan@gmail.com>
2025-05-04 10:51:37 +0700

    Парсинг имени текстуры по параметрам карты

--- sib-game-jam-2025/Scripts/Table/Card.gd
+++ sib-game-jam-2025/Scripts/Table/Card.gd
@@ -2,7 +2,37 @@ class_name Card3D
 
 extends Node3D
 
-enum Type { RED, VIOLET, GREEN, BLUE, GRAY }
+enum Type { KEY, COIN, PUPPET, ALPHABET, SKULL }
+
+var type_representations = {
+	Type.KEY : "key",
+	Type.COIN : "coin",
+	Type.PUPPET : "pu",
+	Type.ALPHABET : "alpha",
+	Type.SKULL : ""
+}
+
+enum Value {
+	AVIAN = 1,
+	BUG = 2,
+	DOG = 3,
+	FROG = 4,
+	LEECHE = 5,
+	TURTLE = 6,
+	TBD = 7,
+	SCULL = 9
+}
+
+var value_representations = {
+	Value.AVIAN : "AVI",
+	Value.BUG : "B",
+	Value.DOG : "D",
+	Value.FROG : "F",
+	Value.LEECHE : "L",
+	Value.TURTLE : "T",
+	Value.TBD : "",
+	Value.SCULL : "scull"
+}
 
 @export var flip_anim_name = "card_flip"
 @export var selection_anim_name = "card_selection"
@@ -12,6 +42,7 @@ enum Type { RED, VIOLET, GREEN, BLUE, GRAY }
 @onready var _anim_player: AnimationPlayer = $AnimationPlayer
 
 var _type: Type
+var _value: Value
 var base_card : Card
 var selectable : bool = false
 var flipable : bool = false
@@ -23,17 +54,25 @@ func init(card : Card):
 	base_card = card
 	match card.color:
 		Card.CARD_COLOR.RED:
-			_type = Type.RED
+			_type = Type.KEY
 		Card.CARD_COLOR.BLUE:
-			_type = Type.BLUE
+			_type = Type.COIN
 		Card.CARD_COLOR.GREEN:
-			_type = Type.GREEN
+			_type = Type.PUPPET
 		Card.CARD_COLOR.VIOLET:
-			_type = Type.VIOLET
+			_type = Type.ALPHABET
 		Card.CARD_COLOR.GREY:
-			_type = Type.GRAY
-	var texture_path = str("res://Sprites/Table/card_", _type_to_low_str(_type),".png")
-	$Body/Front.texture = load(texture_path)
+			_type = Type.SKULL
+	_value = card.value
+	var texture_path = _calc_card_name()
+	$Body/Front.texture = load(_calc_card_name())
+
+func _calc_card_name() -> String:
+	var prefix = "res://Sprites/Table/Cards/"
+	var postfix = "_mute_trans.png"
+	var type_str = type_representations[_type]
+	var value_str = value_representations[_value]
+	return str(prefix, value_str, "_", type_str, postfix)
 
 func move_x(delta_x: float):
 	position += Vector3(delta_x, 0, 0)
