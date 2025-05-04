bcce3b5b1a7621aec57d10bb6a45b905c50c9519
Dron231 <a.igo@g.nsu.ru>
2025-05-04 17:29:25 +0700

    Звуки и PhasesNames

--- sib-game-jam-2025/Scripts/Common/PhasesNames.gd
+++ sib-game-jam-2025/Scripts/Common/PhasesNames.gd
@@ -2,6 +2,10 @@ class_name PhasesNames
 
 extends Node
 
-var dialog_id_learns = "learn"
-var dialog_id_laughs1 = "laugh1"
-var dialog_id_laughs2 = "laugh2"
+var used_dialogs : Array
+
+var dialog_id_learns_bluff = "learn"
+var dialog_id_learns_trust = "learn"
+var dialog_id_correct_bluff_1 = "dialog_correct_bluff_1"
+var dialog_id_correct_bluff_2 = "dialog_correct_bluff_2"
+var dialog_id_correct_bluff_3 = "dialog_correct_bluff_3"
