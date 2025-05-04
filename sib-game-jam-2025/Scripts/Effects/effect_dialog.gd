67d528b9254048a8144b19deb7974ba825917038
Dron231 <a.igo@g.nsu.ru>
2025-05-04 16:38:28 +0700

    Добавил звуки

--- sib-game-jam-2025/Scripts/Effects/effect_dialog.gd
+++ sib-game-jam-2025/Scripts/Effects/effect_dialog.gd
@@ -6,12 +6,11 @@ func _ready() -> void:
 	pass
 
 func start(dialog_id : String) -> void:
-	var dialog_phase = PhaseManager.exact(dialog_id)
-	if !dialog_phase.is_replacement:
-		var scene = load(dialog_phase.scene_name)
-		var scene_instance = scene.instantiate()
-		add_child(scene_instance)
-	#EventBus.end_dialog.connect(finished)
+	var dialog_phase = PhaseManager.start_event(dialog_id)
+	var scene = load(dialog_phase.scene_name)
+	var scene_instance = scene.instantiate()
+	add_child(scene_instance)
+	EventBus.dialog_finished.connect(finished, CONNECT_ONE_SHOT)
 	
 func finished() -> void:
 	EventBusAction.effect_end.emit()
